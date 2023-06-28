3489a2f81a9f68a835795610aa07af285a2727a7
Swpogue <swpogue02@gmail.com>
2023-06-28 14:41:04 -0600

    init

---
+++ AllSpice.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:8080' : ''
+export const useSockets = false
+export const domain = 'dev-zmsf17trpyd7nhu5.us.auth0.com'
+export const clientId = '7GPfU0YWrSDqdocvzTEar4vjEV3o4sjh'
+export const audience = 'http://sandbox.com'
