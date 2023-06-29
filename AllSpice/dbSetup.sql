1e3dd3c7faba10fba7266645d2f4f09bc527aa4a
Swpogue <swpogue02@gmail.com>
2023-06-29 11:25:28 -0600

    controller create

--- AllSpice/dbSetup.sql
+++ AllSpice/dbSetup.sql
@@ -1,3 +1,4 @@
+-- Active: 1687896386573@@SG-codeSandbox-7672-mysql-master.servers.mongodirector.com@3306@myDataBase
 CREATE TABLE IF NOT EXISTS accounts(
   id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
   createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
@@ -6,3 +7,37 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+
+
+CREATE TABLE recipes(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL DEFAULT "misc",
+  img VARCHAR(500) NOT NULL,
+  instructions VARCHAR(500) NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
+
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+
+CREATE TABLE
+    ingredients(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        quantity VARCHAR(255) NOT NULL DEFAULT "1",
+        creatorId VARCHAR(255) NOT NULL,
+        FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+    ) default charset utf8 COMMENT '';
+
+CREATE TABLE
+    favorites(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        creatorId VARCHAR(255) NOT NULL,
+        recipeId VARCHAR(255) NOT NULL,
+        FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+    ) default charset utf8 COMMENT '';
+
+    INSERT INTO albums
+(title, category, img, instructions, creatorId)
+VALUES
+("Pugs", "Asian", "https://images.unsplash.com/photo-1575425186775-b8de9a427e67?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80", "catch dog and roast over a fire", '');
